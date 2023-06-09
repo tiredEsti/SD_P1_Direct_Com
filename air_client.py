0a1d013c421c90957b2bd34807bdae0335cdd846
tiredEsti <alvarezpasjakestela@gmail.com>
2023-06-09 09:14:02 +0200

    Update air_client.py

--- air_client.py
+++ air_client.py
@@ -11,13 +11,13 @@ from meteo_utils import MeteoDataDetector
 channel = grpc.insecure_channel('localhost:50051')
 
 # create a stub (client)
-stub = meteoServer_pb2_grpc.LBServiceStub(channel)
+stub = meteoServer_pb2_grpc.LBStub(channel)
 
 detector = MeteoDataDetector()
 
 # create a valid request message
 air = detector.analyzer_air()
-air_proto = LBServer_pb2.RawMeteoData(temperature=air['temperature'], humidity=air['humidity'], timestamp= datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+air_proto = meteoServer_pb2.RawMeteoData(temperature=air['temperature'], humidity=air['humidity'], timestamp= datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 stub.AddAirData(air_proto)
 
