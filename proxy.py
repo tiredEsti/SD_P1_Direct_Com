e742f5fe1684d84b15a8dee4e83ec26c644ad1f8
tiredEsti <alvarezpasjakestela@gmail.com>
2023-06-12 10:52:07 +0200

    cleaned proxy

--- proxy.py
+++ proxy.py
@@ -1,6 +1,4 @@
-# BEGIN: 8f7a6b1d7c3e
 import redis
-from datetime import datetime
 import time
 import grpc
 
@@ -11,7 +9,7 @@ import meteoServer_pb2_grpc
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 # Tumbling window size in seconds
-Y = 5
+Y = 10
 
 # gRPC server connection
 channel = grpc.insecure_channel('localhost:50056')
@@ -21,41 +19,26 @@ stub = meteoServer_pb2_grpc.TerminalServiceStub(channel)
 def calculate_mean(lst):
     return sum(lst) / len(lst)
 
-# Main loop
-while True:
-    # Get coefficients from Redis list within the tumbling window
-    wellc = []
-    pollc = []
-    wcoeff = r.rpop('meteo')
-    pcoeff = r.rpop('poll')
-    if wcoeff is None:
-        if pcoeff is None:
-            time.sleep(Y)
-            break
-
-    pollc.append(pcoeff.decode().split(':')[1].strip(')'))
-    wellc.append(wcoeff.decode().split(':')[1].strip(')')
-    
-    timestampwell = wcoeff.decode().split(':')[0].strip('(')
-    timestamppoll = pcoeff.decode().split(':')[0].strip('(')
+# Function to get data from Redis return calculate mean and timestamp
+def get_data(type):
+    coeff = r.rpop(type)
+    if coeff is None:
+        return [None, None]
+    timestamp = coeff.decode().split(':')[0].strip('(')
     while True:
-        wcoeff = r.rpop('meteo')
-        pcoeff = r.rpop('poll')
-        if wcoeff is None
-            if pcoeff is None:
-                break
-        
-        if wcoeff is not None:
-            wellc.append(float(wcoeff.decode().split(':')[1].strip(')')))
-        if pcoeff is not None:
-            pollc.append(float(pcoeff.decode().split(':')[1].strip(')')))
-        
+        value = float(coeff.decode().split(':')[1].strip(')'))
+        list.append(value)
+        coeff = r.rpop(type)
+        if coeff is None:
+            break
+    return [calculate_mean(list), timestamp]
 
-    # Calculate mean of coefficients
-    wmean = calculate_mean(wellc)
-    pmean = calculate_mean(pollc)
+# Main loop
+while True:   
+    wdata = get_data('meteo')
+    pdata = get_data('poll')
     # Send mean to connected terminals
-    response = stub.AddTerminalData(meteoServer_pb2.TerminalData(well = wmean, poll = pmean, timestampWell=timestampwell, timestampPoll=timestamppoll))
+    response = stub.AddTerminalData(meteoServer_pb2.TerminalData(well = wdata[0], poll = pdata[0], timestampWell = wdata[1], timestampPoll = pdata[1]))
     print("Data retrieved from Redis and sent to terminals")
     time.sleep(Y)
 
