db7f1811018171e1e0b2f0daa5ef97bf4523ad50
tiredEsti <alvarezpasjakestela@gmail.com>
2023-06-12 19:08:16 +0200

    Update terminal_server.py

--- terminal_server.py
+++ terminal_server.py
@@ -3,14 +3,8 @@ from concurrent import futures
 import time
 from datetime import datetime
 
-import dash
-from dash.dependencies import Output, Event
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly
-import random
-import plotly.graph_objs as go
-from collections import deque
+
+import plotly.graph_objects as go
 
 # import the generated classes
 import meteoServer_pb2
@@ -45,47 +39,10 @@ fig.layout.title = 'Wellness and pollution data'
 fig.layout.xaxis.title = 'Time'
 fig.layout.yaxis.title = 'Coefficient'
 ctr = 0
-X = deque(maxlen=20)
-X.append(1)
-Y = deque(maxlen=20)
-Y.append(1)
-
-app = dash.Dash(__name__)
-app.layout = html.Div(
-    [
-        dcc.Graph(id='live-graph', animate=True),
-        dcc.Interval(
-            id='graph-update',
-            interval=1*1000
-        ),
-    ]
-)
-@app.callback(Output('live-graph', 'figure'),
-              events=[Event('graph-update', 'interval')])
-
-def update_graph_scatter():
-    X.append(X[-1]+1)
-    Y.append(Y[-1]+Y[-1]*random.uniform(-0.1,0.1))
-
-    data = plotly.graph_objs.Scatter(
-            x=list(X),
-            y=list(Y),
-            name='Scatter',
-            mode= 'lines+markers'
-            )
-
-    return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
-                                                yaxis=dict(range=[min(Y),max(Y)]),)}
-
-try:
-    while True:
-        app.run_server(debug=True)
-except KeyboardInterrupt:
-    server.stop(0)
 
 # since server.start() will not block,
 # a sleep-loop is added to keep alive
-""" try:
+try:
     while True:
         with fig.batch_update():
             fig.data[0].x = terminal_service.get_x()
@@ -94,4 +51,3 @@ except KeyboardInterrupt:
         ctr += 1
 except KeyboardInterrupt:
     server.stop(0)
- """
