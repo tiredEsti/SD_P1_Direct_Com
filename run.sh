d3245cb7dbfb720169dff4f2bfef3c543ac898fa
tiredEsti <estela.alvarez@estudiants.urv.cat>
2023-06-13 15:04:27 +0200

    kk

--- run.sh
+++ run.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-python3 -m grpc_tools.protoc --proto_path=. --grpc_python_out=. --pyi_out=. --python_out=. ./meteoServer.proto
-
-python3 LB_server.py &
-python3 meteo_server.py &
-python3 terminal_server.py &
-python3 terminal_server.py &
-
-sleep 5
-
 while :
 do
     python3 air_client.py &
     python3 poll_client.py &
 done
-python3 proxy.py &
-
-sleep 60
-
-killall python3
